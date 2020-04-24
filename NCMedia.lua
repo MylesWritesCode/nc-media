6be43148d0ac6ed426bd5f551b6cb546e0e75666
Myles Berueda <myles.berueda@codemithril.com>
2020-04-23 17:19:57 -1000

    initial commit

---
+++ NCMedia.lua
@@ -0,0 +1,10 @@
+local ADDON = ...
+local PATH = "Interface\\AddOns\\NCMedia\\Sounds\\"
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- LSM:Register("sound", "NC: <name>", [[Interface\Addons\NCMedia\Sounds\<file>.mp3]])
+
+LSM:Register("sound", "NC: Conscript", [[Interface\Addons\NCMedia\Sounds\nc-conscript.mp3]])
+LSM:Register("sound", "NC: Away From X", [[Interface\Addons\NCMedia\Sounds\nc-get-away-from-x.mp3]])
+LSM:Register("sound", "NC: Phoopsie", [[Interface\Addons\NCMedia\Sounds\nc-phoopsie.mp3]])
+LSM:Register("sound", "NC: Phowo", [[Interface\Addons\NCMedia\Sounds\nc-phowo.mp3]])
